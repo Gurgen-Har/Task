7d0b422323cab46f1c34ae5b0c8fb668f42640bf
Gurgen-Har <harutyunyan.gurgen.job@gmail.com>
2023-09-16 19:46:37 +0400

    First

---
+++ src/Manager.java
@@ -0,0 +1,15 @@
+public class Manager {
+    public void getAllTasks(){}
+
+    public void deleteAllTasks(){}
+
+    public void getTasksFromId(){}
+
+    public void createTasks(){}
+
+    public void refresh(){}
+
+    public void deleteFromId(){}
+
+    
+}
